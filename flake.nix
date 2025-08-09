8be239810f98760f266cef1ada23a74806831a73
Andrei Martillo <ddrhckrzz@gmail.com>
2025-08-09 14:43:20 +0800

    Initial commit. Added flakes. yum.

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Drei's Generic Desktop System";
+
+  inputs = {
+    # System packages
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations.ddrhckrzz-asusl-nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [ ./configuration.nix ];
+    };
+  };
+}

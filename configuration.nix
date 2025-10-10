766e39261829530f2804a8537924e051de8572a2
Andrei Martillo <ddrhckrzz@gmail.com>
2025-10-11 00:08:12 +0800

    Fixed bluetooth (for real) Added Official Discord client (with OpenASAR and Vencord enabled)

--- configuration.nix
+++ configuration.nix
@@ -109,6 +109,29 @@
     nvidiaBusId = "PCI:1:0:0";
   };
 
+  # Bluetooth
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      General = {
+        # Shows battery charge of connected devices on supported
+        # Bluetooth adapters. Defaults to 'false'.
+        Experimental = true;
+        # When enabled other devices can connect faster to us, however
+        # the tradeoff is increased power consumption. Defaults to
+        # 'false'.
+        FastConnectable = true;
+      };
+      Policy = {
+        # Enable all controllers when they are found. This includes
+        # adapters present on start as well as adapters that are plugged
+        # in later on. Defaults to 'true'.
+        AutoEnable = true;
+      };
+    };
+  };
+
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
@@ -245,6 +268,10 @@
     
     # Desktop Apps
     pkgs.vesktop 			        # Discord
+    (discord.override {
+      withOpenASAR = true;
+      withVencord = true;
+    })
     pkgs.gparted			        # Partition Manager
     pkgs.prismlauncher		  	# Prism Launcher for Minecraft
     hardinfo2                 # System information and benchmarks for Linux systems

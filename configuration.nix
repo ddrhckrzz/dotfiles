74686c5b749a52fed8bbaf217528503f9ebe8124
Andrei Martillo <ddrhckrzz@gmail.com>
2026-02-11 14:56:30 +0800

    added another config to v4l2loopback to see if it fixes it idk

--- configuration.nix
+++ configuration.nix
@@ -36,7 +36,7 @@
       # exclusive_caps: Skype, Zoom, Teams etc. will only show device when actually streaming
       # card_label: Name of virtual camera, how it'll show up in Skype, Zoom, Teams
       # https://github.com/umlaeute/v4l2loopback
-      options v4l2loopback exclusive_caps=1 card_label="Virtual Camera"
+      options v4l2loopback nr_devices=2 exclusive_caps=1 card_label="Virtual Camera"
     '';
   };
 
